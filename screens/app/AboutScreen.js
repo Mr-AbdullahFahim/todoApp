e70ec513dbc2d467b16ec632bda6b836b00eb32b
Naveendanj <naveenhettiwaththa@gmail.com>
2024-09-20 18:46:41 +0530

    added latest about page ui changes

--- screens/app/AboutScreen.js
+++ screens/app/AboutScreen.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, Text, SafeAreaView, TextInput, Alert } from 'react-na
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import AsyncStorageService from '../../services/AsyncStorageService';
-
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function AboutAppAScreen({navigation}){
 
@@ -28,8 +28,27 @@ export default function AboutAppAScreen({navigation}){
                         <Text style={styles.headerText}>App Settings and Info 🌍</Text>
                     </View>
                 </View>
+                
+                <Text style={[styles.bodyText , {marginTop : 10}]}>Profile Info</Text>
 
-                <Text style={[styles.bodyText , {marginTop : 0}]}>App Info</Text>
+                <View style={{ marginBottom : 10 , padding : 13 , backgroundColor : '#181818' , borderRadius : 8 ,  marginTop : 10 }}>
+                    
+                    
+                    <View style={{ display : 'flex' , flexDirection: 'row' , gap : 10 }}>
+                        <AntDesign name="delete" size={24} color="white" />
+                        <Text style={styles.titleText}>Profile Information</Text>
+                    </View>
+                    <Text style={styles.bodyText}>This action is irreversible. It will delete your data including tasks and other data.</Text>
+
+                    <View style={{ display :'flex' , width : '100%' ,  flexDirection : 'row' , justifyContent : 'flex-end' }}>
+                        <TouchableOpacity onPress={resetData} style={{ marginTop : 10 , borderRadius : 8,  padding : 12 , paddingHorizontal : 25 , backgroundColor : 'rgba(63,19,22,1)' }}>
+                            <Text style={{ color :'#EE716D' , fontSize : 16 }}>Delete Data</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+
+                <Text style={[styles.bodyText , {marginTop : 10}]}>App Info</Text>
 
                 <View style={{ borderWidth : 1 , borderColor : 'rgba(255,255,255,0.2)' , marginTop : 13  , backgroundColor : '#181818' , borderRadius : 8 }}>
                     
